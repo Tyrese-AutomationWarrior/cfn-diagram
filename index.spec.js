Tyrese-AutomationWarrior/cfn-diagram

commit 4aa359c13d31f5d665ec93eb174c237ed72ba74f

Fixed ciMode not writing draw.io diagrams to filesystem (#28)
Co-authored-by: Adrian Nieto Perez <anietoperez@sequel.com>

graph/MxGenerator.js
@@ -250,9 +250,11 @@ async function generate(cmd, template) {
   let resourceNames = { answer: resources };
   let edgeMode = { answer: "On" };
   let actionChoice = {};
-  console.log("Writing diagram to ./template.drawio");
+  console.log("Diagram will be written to " + cmd.outputFile);
 
   if (ciMode) {
+    const xml = renderTemplate(template);
+    fs.writeFileSync(cmd.outputFile, xml);
     return;
   }
 

commit 46ecb1520068d6be6482c8c528f54e976b745129

Fix/not working ci mode for draw io (#29)
* Fixed ci-mode drawio diagram generation broken after #28

* Added index.js tests to validate ci mode

Co-authored-by: Adrian Nieto Perez <anietoperez@sequel.com>

graph/MxGenerator.js
@@ -248,9 +248,14 @@ async function generate(cmd, template) {
   types = [...new Set(types)].sort();
   let resourceTypes = { answer: types };
   let resourceNames = { answer: resources };
-  let edgeMode = { answer: "On" };
+  let edgeMode = { answer: "On" }; 
+  filterConfig.resourceNamesToInclude = resourceNames.answer;
+  filterConfig.resourceTypesToInclude = resourceTypes.answer;
+  filterConfig.edgeMode = edgeMode.answer;
+
   let actionChoice = {};
   console.log("Diagram will be written to " + cmd.outputFile);
+
 
   if (ciMode) {
     const xml = renderTemplate(template);

index.spec.js
@@ -1,0 +1,36 @@
+let os = require('os');
+let path = require('path');
+let exec = require('child_process').exec;
+
+test('Code should be 0', async () => {
+    jest.setTimeout(30000);
+
+    console.log("output path set to " + os.tmpdir() + '/template.drawio')
+    let result = await cli(['draw.io', '--template-file', './tests/template.json', '--output-file', os.tmpdir() + '/template.drawio', '--ci-mode' ], '.');
+    console.log(result.stdout)
+    console.log(result.stderr)
+    expect(result.code).toBe(0);
+
+    console.log("output path set to " + os.tmpdir())
+    result = await cli(['html', '--template-file', './tests/template.json', '--output-path', os.tmpdir(), '--ci-mode'], '.');
+    console.log(result.stdout)
+    console.log(result.stderr)
+    expect(result.code).toBe(0);
+
+})
+
+
+function cli(args, cwd) {
+    return new Promise(resolve => {
+        exec(`node ${path.resolve('./index.js')} ${args.join(' ')}`,
+            { cwd },
+            (error, stdout, stderr) => {
+                resolve({
+                    code: error && error.code ? error.code : 0,
+                    error,
+                    stdout,
+                    stderr
+                })
+            })
+    })
+}